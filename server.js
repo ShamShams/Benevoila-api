f84f1384dc67d902848fc02493f916a4b889dff1
sremtoula@simplon.co <sremtoula@simplon.co>
2018-06-14 12:08:00 +0200

    Add router

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const routes = require('./routers/router');
+
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000');
