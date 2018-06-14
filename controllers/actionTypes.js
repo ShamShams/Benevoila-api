e5e55892524c5c80693514aa95f4762936a24e60
sremtoula@simplon.co <sremtoula@simplon.co>
2018-06-14 15:33:32 +0200

    Add actionTypes getOne method

--- controllers/actionTypes.js
+++ controllers/actionTypes.js
@@ -2,63 +2,79 @@ const SQL = require('sql-template-strings');
 
 const client = require('../db');
 
-module.exports = {
-
-    getAll: async (req, res) => {
-        const querySql = SQL`
-            SELECT
-                *
-            FROM
-                action_types
-        `
-        let actionTypes = null;
-        try {
-            actionTypes = await client.query(querySql);
-        } catch (error) {
-            res.status(500).send(new Error("Erreur dans la récupération des types d'action", error));
-        }
-        return res.status(200).send(actionTypes.rows);
-    },
-
-    createOne: async (req, res) => {
-        const { name, description } = req.body;
-        const querySql = SQL`
-            INSERT INTO action_types (
-                name,
-                description
-            ) VALUES (
-                ${name},
-                ${description}
-            ) RETURNING *
-        `
-        let newActionType = null;
-        try {
-            newActionType = await client.query(querySql);
-        } catch (error) {
-            res.status(500).send(error);
-        }
-        return res.status(200).send(newActionType.rows);
-    },
-
-    updateOne: async (req, res) => {
-        const { name, description } = req.body;
-        const querySql = SQL`
-            UPDATE
-                action_types
-            SET
-                name=${name},
-                description=${description}
-            WHERE
-                action_type_id=${req.params.id}
-            RETURNING *
-        `
-        let updatedActionType = null;
-        try {
-            updatedActionType = await client.query(querySql);
-        } catch (error) {
-            res.status(500).send(error);
-        }
-        return res.status(200).send(updatedActionType.rows);
+const getAll = async (req, res) => {
+    const querySql = SQL`
+        SELECT
+            *
+        FROM
+            action_types
+    `
+    let actionTypes = null;
+    try {
+        actionTypes = await client.query(querySql);
+    } catch (error) {
+        res.status(500).send(new Error("Erreur dans la récupération des types d'action", error));
     }
-
+    return res.status(200).send(actionTypes.rows);
 };
+
+const getOne = async (req, res) => {
+    const querySql = SQL`
+        SELECT
+            *
+        FROM
+            action_types
+        WHERE
+            action_type_id=${req.params.id}
+    `
+    let actionType = null;
+    try {
+        actionType = await client.query(querySql);
+    } catch (error) {
+        res.status(500).send(new Error("Erreur dans la récupération des types d'action", error));
+    }
+    return res.status(200).send(actionType.rows);
+};
+
+const createOne = async (req, res) => {
+    const { name, description } = req.body;
+    const querySql = SQL`
+        INSERT INTO action_types (
+            name,
+            description
+        ) VALUES (
+            ${name},
+            ${description}
+        ) RETURNING *
+    `
+    let newActionType = null;
+    try {
+        newActionType = await client.query(querySql);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    return res.status(200).send(newActionType.rows);
+};
+
+const updateOne = async (req, res) => {
+    const { name, description } = req.body;
+    const querySql = SQL`
+        UPDATE
+            action_types
+        SET
+            name=${name},
+            description=${description}
+        WHERE
+            action_type_id=${req.params.id}
+        RETURNING *
+    `
+    let updatedActionType = null;
+    try {
+        updatedActionType = await client.query(querySql);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    return res.status(200).send(updatedActionType.rows);
+};
+
+module.exports = { getAll, getOne, createOne, updateOne };
--- routers/actionTypesRouter.js
+++ routers/actionTypesRouter.js
@@ -5,6 +5,7 @@ const actionTypes = require('../controllers/actionTypes');
 const router = new Router();
 
 router.get('/', actionTypes.getAll);
+router.get('/:id', actionTypes.getOne);
 router.post('/', actionTypes.createOne);
 router.put('/:id', actionTypes.updateOne);
 
