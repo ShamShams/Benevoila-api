003fca6bdb03d791de3f937b89a19ca3e0369668
sremtoula@simplon.co <sremtoula@simplon.co>
2018-11-02 15:45:17 +0100

    Change server message logged

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(cors());
 app.use(routes);
 
 app.listen(port, () => {
-    console.log(`>>>>>> Server is running on port ${port} <<<<<<`);
+    console.log(`...Server is running on port ${port}...`);
 });
